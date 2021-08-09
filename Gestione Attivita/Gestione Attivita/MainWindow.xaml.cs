ecf25bbbe9b40be2138eef1e4b54a00a84031b0e
lucacatt <lucamarino0364@gmail.com>
2021-08-09 10:22:18 +0200

    Create MainWindow.xaml.cs

---
+++ Gestione Attivita/Gestione Attivita/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using OpenHardwareMonitor.Hardware;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gestione_Attivita
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //https://www.youtube.com/watch?v=Lg_geJrATrc&ab_channel=FoxLearn tutorial https://stackoverflow.com/questions/1195112/how-can-i-get-the-cpu-temperature http://www.lattepanda.com/topic-f11t3004.html
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
