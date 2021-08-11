45f5745f9caccf708b64402a079f9a66a3baffb3
lucacatt <lucamarino0364@gmail.com>
2021-08-11 10:56:27 +0200

    monitoraggio cpu

--- Gestione Attivita/Gestione Attivita/Gestore.Designer.cs
+++ Gestione Attivita/Gestione Attivita/Gestore.Designer.cs
@@ -30,11 +30,107 @@ namespace Gestione_Attivita
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.metroLabel = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressBarCpu = new MetroFramework.Controls.MetroProgressBar();
+            this.lblCpu = new MetroFramework.Controls.MetroLabel();
+            this.pCPU = new System.Diagnostics.PerformanceCounter();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.gradi = new MetroFramework.Controls.MetroLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pCPU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // metroLabel
+            // 
+            this.metroLabel.AutoSize = true;
+            this.metroLabel.Location = new System.Drawing.Point(23, 60);
+            this.metroLabel.Name = "metroLabel";
+            this.metroLabel.Size = new System.Drawing.Size(33, 19);
+            this.metroLabel.TabIndex = 0;
+            this.metroLabel.Text = "Cpu";
+            // 
+            // metroProgressBarCpu
+            // 
+            this.metroProgressBarCpu.Location = new System.Drawing.Point(110, 56);
+            this.metroProgressBarCpu.Name = "metroProgressBarCpu";
+            this.metroProgressBarCpu.Size = new System.Drawing.Size(613, 23);
+            this.metroProgressBarCpu.TabIndex = 1;
+            // 
+            // lblCpu
+            // 
+            this.lblCpu.AutoSize = true;
+            this.lblCpu.Location = new System.Drawing.Point(729, 60);
+            this.lblCpu.Name = "lblCpu";
+            this.lblCpu.Size = new System.Drawing.Size(31, 19);
+            this.lblCpu.TabIndex = 2;
+            this.lblCpu.Text = "0 %";
+            // 
+            // pCPU
+            // 
+            this.pCPU.CategoryName = "Processor";
+            this.pCPU.CounterName = "% Processor Time";
+            this.pCPU.InstanceName = "_Total";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(23, 111);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "CPU";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(571, 300);
+            this.chart1.TabIndex = 3;
+            // 
+            // gradi
+            // 
+            this.gradi.AutoSize = true;
+            this.gradi.Location = new System.Drawing.Point(568, 111);
+            this.gradi.Name = "gradi";
+            this.gradi.Size = new System.Drawing.Size(73, 19);
+            this.gradi.TabIndex = 4;
+            this.gradi.Text = "ighujtsrnbe";
+            // 
+            // Gestore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Gestore";
+            this.ClientSize = new System.Drawing.Size(787, 448);
+            this.Controls.Add(this.gradi);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.lblCpu);
+            this.Controls.Add(this.metroProgressBarCpu);
+            this.Controls.Add(this.metroLabel);
+            this.Name = "Gestore";
+            this.Text = "Gestore Attività";
+            this.Load += new System.EventHandler(this.Gestore_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pCPU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Timer timer;
+        private MetroFramework.Controls.MetroLabel metroLabel;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBarCpu;
+        private MetroFramework.Controls.MetroLabel lblCpu;
+        private System.Diagnostics.PerformanceCounter pCPU;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private MetroFramework.Controls.MetroLabel gradi;
     }
 }
--- Gestione Attivita/Gestione Attivita/Gestore.cs
+++ Gestione Attivita/Gestione Attivita/Gestore.cs
@@ -10,11 +10,25 @@ using System.Windows.Forms;
 
 namespace Gestione_Attivita
 {
-    public partial class Gestore : Form
+    public partial class Gestore : MetroFramework.Forms.MetroForm
     {
         public Gestore()
         {
             InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            float fcpu = pCPU.NextValue();
+            metroProgressBarCpu.Value = (int)fcpu;
+            lblCpu.Text = string.Format("{0:0.00}%",fcpu);
+            chart1.Series["CPU"].Points.AddY(fcpu);
+            gradi.Text = UpdateVisitor.GetCpuInfo();
+        }
+
+        private void Gestore_Load(object sender, EventArgs e)
+        {
+            timer.Start();
         }
     }
 }
