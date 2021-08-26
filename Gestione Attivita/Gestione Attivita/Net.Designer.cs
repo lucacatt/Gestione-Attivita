db9ca0cb6b8619aa420992d4f1a0748bca1da6f3
lucacatt <lucamarino0364@gmail.com>
2021-08-26 11:44:36 +0200

    rete

--- Gestione Attivita/Gestione Attivita/Net.Designer.cs
+++ Gestione Attivita/Gestione Attivita/Net.Designer.cs
@@ -30,32 +30,149 @@ namespace Gestione_Attivita
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.prova = new MetroFramework.Controls.MetroLabel();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.cmbInterface = new MetroFramework.Controls.MetroComboBox();
+            this.lblBytesSent = new MetroFramework.Controls.MetroLabel();
+            this.lblBytesReceived = new MetroFramework.Controls.MetroLabel();
+            this.lblSpeed = new MetroFramework.Controls.MetroLabel();
+            this.lblInterfaceType = new MetroFramework.Controls.MetroLabel();
+            this.lblDownload = new MetroFramework.Controls.MetroLabel();
+            this.lblUpload = new MetroFramework.Controls.MetroLabel();
             this.timer = new System.Windows.Forms.Timer(this.components);
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
-            // prova
+            // cmbInterface
             // 
-            this.prova.AutoSize = true;
-            this.prova.Location = new System.Drawing.Point(291, 150);
-            this.prova.Name = "prova";
-            this.prova.Size = new System.Drawing.Size(81, 19);
-            this.prova.TabIndex = 0;
-            this.prova.Text = "metroLabel1";
+            this.cmbInterface.FormattingEnabled = true;
+            this.cmbInterface.ItemHeight = 23;
+            this.cmbInterface.Location = new System.Drawing.Point(23, 63);
+            this.cmbInterface.Name = "cmbInterface";
+            this.cmbInterface.Size = new System.Drawing.Size(121, 29);
+            this.cmbInterface.TabIndex = 0;
+            this.cmbInterface.UseSelectable = true;
+            // 
+            // lblBytesSent
+            // 
+            this.lblBytesSent.AutoSize = true;
+            this.lblBytesSent.Location = new System.Drawing.Point(120, 95);
+            this.lblBytesSent.Name = "lblBytesSent";
+            this.lblBytesSent.Size = new System.Drawing.Size(16, 19);
+            this.lblBytesSent.TabIndex = 1;
+            this.lblBytesSent.Text = "0";
+            // 
+            // lblBytesReceived
+            // 
+            this.lblBytesReceived.AutoSize = true;
+            this.lblBytesReceived.Location = new System.Drawing.Point(120, 114);
+            this.lblBytesReceived.Name = "lblBytesReceived";
+            this.lblBytesReceived.Size = new System.Drawing.Size(16, 19);
+            this.lblBytesReceived.TabIndex = 2;
+            this.lblBytesReceived.Text = "0";
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Location = new System.Drawing.Point(23, 133);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(54, 19);
+            this.lblSpeed.TabIndex = 3;
+            this.lblSpeed.Text = "Velocità";
+            // 
+            // lblInterfaceType
+            // 
+            this.lblInterfaceType.AutoSize = true;
+            this.lblInterfaceType.Location = new System.Drawing.Point(23, 152);
+            this.lblInterfaceType.Name = "lblInterfaceType";
+            this.lblInterfaceType.Size = new System.Drawing.Size(113, 19);
+            this.lblInterfaceType.TabIndex = 4;
+            this.lblInterfaceType.Text = "Tipo di interfaccia";
+            // 
+            // lblDownload
+            // 
+            this.lblDownload.AutoSize = true;
+            this.lblDownload.Location = new System.Drawing.Point(23, 171);
+            this.lblDownload.Name = "lblDownload";
+            this.lblDownload.Size = new System.Drawing.Size(68, 19);
+            this.lblDownload.TabIndex = 5;
+            this.lblDownload.Text = "Download";
+            // 
+            // lblUpload
+            // 
+            this.lblUpload.AutoSize = true;
+            this.lblUpload.Location = new System.Drawing.Point(23, 190);
+            this.lblUpload.Name = "lblUpload";
+            this.lblUpload.Size = new System.Drawing.Size(52, 19);
+            this.lblUpload.TabIndex = 6;
+            this.lblUpload.Text = "Upload";
             // 
             // timer
             // 
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 114);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(79, 19);
+            this.metroLabel1.TabIndex = 7;
+            this.metroLabel1.Text = "Byte ricevuti";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(23, 95);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(71, 19);
+            this.metroLabel2.TabIndex = 8;
+            this.metroLabel2.Text = "Byte inviati";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(276, 95);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "INVIATI";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "RICEVUTI";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(511, 300);
+            this.chart1.TabIndex = 9;
             // 
             // Net
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.prova);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.lblUpload);
+            this.Controls.Add(this.lblDownload);
+            this.Controls.Add(this.lblInterfaceType);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.lblBytesReceived);
+            this.Controls.Add(this.lblBytesSent);
+            this.Controls.Add(this.cmbInterface);
             this.Name = "Net";
-            this.Text = "Rete";
-            this.Load += new System.EventHandler(this.Net_Load);
+            this.Text = "Net";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -63,7 +180,16 @@ namespace Gestione_Attivita
 
         #endregion
 
-        private MetroFramework.Controls.MetroLabel prova;
+        private MetroFramework.Controls.MetroComboBox cmbInterface;
+        private MetroFramework.Controls.MetroLabel lblBytesSent;
+        private MetroFramework.Controls.MetroLabel lblBytesReceived;
+        private MetroFramework.Controls.MetroLabel lblSpeed;
+        private MetroFramework.Controls.MetroLabel lblInterfaceType;
+        private MetroFramework.Controls.MetroLabel lblDownload;
+        private MetroFramework.Controls.MetroLabel lblUpload;
         private System.Windows.Forms.Timer timer;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- Gestione Attivita/Gestione Attivita/Net.cs
+++ Gestione Attivita/Gestione Attivita/Net.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,31 +15,52 @@ namespace Gestione_Attivita
 {
     public partial class Net : MetroFramework.Forms.MetroForm
     {
-        Thread t;
-        string g = "";
+        private NetworkInterface[] nicArr;
         public Net()
         {
             InitializeComponent();
-        }
-        void start()
-        {
-            while (true)
-                g = UpdateVisitor.GetNetInfo();
+            InitializeNetworkInterface();
+            lblBytesSent.Text = "0";
+            lblBytesReceived.Text = "0";
+            timer.Start();
         }
         private void timer_Tick(object sender, EventArgs e)
         {
-            PerformanceCounterCategory pcg = new PerformanceCounterCategory("Network Interface");
-            string instance = pcg.GetInstanceNames()[0];
-            PerformanceCounter pcsent = new PerformanceCounter("Network Interface", "Bytes Sent/sec", instance);
-            PerformanceCounter pcreceived = new PerformanceCounter("Network Interface", "Bytes Received/sec", instance);
-            prova.Text = "Bytes Sent: " + pcsent.NextValue() / 1024 + "\n";
-            prova.Text += "Bytes Received: " + pcreceived.NextValue() / 1024;
+            UpdateNetworkInterface();
         }
         private void Net_Load(object sender, EventArgs e)
         {
-            timer.Start();
-            t = new Thread(start);
-            t.Start();
+
+        }
+        private void InitializeNetworkInterface()
+        {
+            nicArr = NetworkInterface.GetAllNetworkInterfaces();
+            for (int i = 0; i < nicArr.Length; i++)
+                cmbInterface.Items.Add(nicArr[i].Name);
+            cmbInterface.SelectedIndex = 0;
+        }
+
+        private void UpdateNetworkInterface()
+        {
+            NetworkInterface nic = nicArr[cmbInterface.SelectedIndex];
+            IPv4InterfaceStatistics interfaceStats = nic.GetIPv4Statistics();
+            int bytesSentSpeed = Convert.ToInt32(interfaceStats.BytesSent - double.Parse(lblBytesSent.Text) / 1024);
+            int bytesReceivedSpeed = Convert.ToInt32(interfaceStats.BytesReceived - double.Parse(lblBytesReceived.Text) / 1024);
+            // Update the labels
+            lblSpeed.Text = "Velocita " + nic.Speed.ToString();
+            lblInterfaceType.Text = "Tipo di interfaccia " + nic.NetworkInterfaceType.ToString();
+            lblSpeed.Text = "Velocita " + nic.Speed.ToString();
+            lblBytesReceived.Text = interfaceStats.BytesReceived.ToString();
+            lblBytesSent.Text = interfaceStats.BytesSent.ToString();
+            lblUpload.Text = "Upload " + bytesSentSpeed.ToString() + " KB/s";
+            lblDownload.Text = "Download " + bytesReceivedSpeed.ToString() + " KB/s";
+            chart1.Series["RICEVUTI"].Points.AddY(lblBytesReceived.Text);
+            chart1.Series["INVIATI"].Points.AddY(lblBytesSent.Text);
+        }
+
+        private void Net_Load_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
